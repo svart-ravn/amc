1413e60fd3b34c75030654bfff042139ea7f72a1
svart-ravn <srv.db.dev@gmail.com>
2016-07-11 13:43:20 -0700

    adding todo list

--- amc.go
+++ amc.go
@@ -6,5 +6,27 @@ import (
 
 
 func main(){
-   fmt.Println("ok")
+   // fmt.Println("ok")
+   // TODO:
+   // 
+   // 0. get list of configs
+   // 
+   //       amc cget --cluster UAT
+   // 
+   // 1. git diff between clusters
+   // 
+   //        amc cdiff --clusters-list UAT,PROD --configs-list HDFS,HIVE       
+   // 
+   // 
+   // 2. find changes for selected variable(s)
+   // 
+   //        amc log --cluster UAT --parameter dfs.namenode.thread.count
+   // 
+   // 3. diff between 2 version of selected configs
+   // 
+   //        amc vdiff --version1 v55|head --version2 v54|-1
+   // 
+   // 
+   // 4. ?
+   // 
 }
